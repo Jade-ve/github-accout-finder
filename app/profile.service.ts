c999064db6f4cee6f044cc81ad4a697b20229d6d
jadeve <susan.jadeve@gmail.com>
2021-09-01 00:08:20 +0300

    made changes on the (.map)

--- app/profile.service.ts
+++ app/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map'
+import 'rxjs-compat/add/operator/map'
 import { environment } from 'src/environments/environment';
 
 
@@ -20,20 +20,19 @@ export class ProfileService {
 
   
    
+   getProfileInfo(){
+    return this.httpClient.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    Object((res: any) => {
+      return res
+     });
+   }
 
-  getProfileInfo(){
-     return this.httpClient.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-     .map((res: any) => {
+   getProfileRepos(){
+     return this.httpClient.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     Object((res: any) => {
        return res
       });
-    }
-
-    getProfileRepos(){
-      return this.httpClient.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-      .map((res: any) => {
-        return res
-       });
-      }
+     }
        updateProfile(username:string){
          this.username = username;
        }
