19754a957fdfdf20113de49c06ccdfcb1e6c86ce
jadeve <susan.jadeve@gmail.com>
2021-08-31 23:41:27 +0300

    importing the forms module

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,22 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { PersonalComponent } from './personal/personal.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile.service';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PersonalComponent } from './personal/personal.component';
+import { CapsPipe } from './caps.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
+    ProfileComponent,
     PersonalComponent,
-    ProfileComponent
+    CapsPipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ ProfileService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/profile.service.ts
+++ app/profile.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map'
 import { environment } from 'src/environments/environment';
+
 
 @Injectable()
 export class ProfileService {
 
    private username:string;
    private clientid = '89ce624fda7d1d49555c';
-   private clientsecret = "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";
+   private clientsecret = "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";
 
 
 
   constructor(private httpClient:HttpClient) {
     console.log("service is now ready");
-    this.username = 'Ephrao1'
+    this.username = 'Jade-ve'
    }
 
   
    
 
-   getProfileInfo(){
+  getProfileInfo(){
      return this.httpClient.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
      .map((res: any) => {
        return res
@@ -38,3 +39,4 @@ export class ProfileService {
        }
      
     }
+   
